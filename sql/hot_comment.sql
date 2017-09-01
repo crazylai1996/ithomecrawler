e69e88ea7a33a1c756d86024af919b3697d4c133
crazylai1996 <1284753520@qq.com>
2017-09-01 17:27:33 +0800

    update *.sql

--- sql/hot_comment.sql
+++ sql/hot_comment.sql
@@ -13,6 +13,10 @@ File Encoding         : 65001
 Date: 2017-08-27 19:11:12
 */
 
+CREATE DATABASE `ithome` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+USE ithome;
+
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -22,12 +26,12 @@ DROP TABLE IF EXISTS `hot_comment`;
 CREATE TABLE `hot_comment` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `vCommentId` varchar(20) DEFAULT NULL,
-  `vUser` varchar(50) CHARACTER SET utf8mb4 DEFAULT NULL,
-  `vComment` text CHARACTER SET utf8mb4,
+  `vUser` varchar(50) DEFAULT NULL,
+  `vComment` text ,
   `iUp` int(11) DEFAULT NULL,
   `iDown` int(11) DEFAULT NULL,
   `vPosandTime` varchar(50) DEFAULT NULL,
   `vMobile` varchar(50) DEFAULT NULL,
   `vArticleUrl` varchar(80) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
